f5ed4c2eef9d288c33b58471ec5f31b7b1e7bb68
adhishanand9 <adhishanand9@gmail.com>
2024-04-08 18:14:31 +0530

    Update code for 4.2C

--- server.js
+++ server.js
@@ -124,7 +124,93 @@ app.get("/division", (req,res)=>{
       }
 });
 
-const port=3000;
+const power= (n1,n2) => { 
+    return Math.pow(n1,n2);       
+}
+app.get("/power", (req,res)=>{ 
+    try{
+    const n1= parseFloat(req.query.n1); 
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) { 
+        logger.error("n1 is incorrectly defined"); 
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) { 
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    logger.info('Parameters '+n1+' and '+n2+' received for exponentiation'); 
+    const result = power(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+const sqrt= (n1,n2) => { 
+    return Math.sqrt(n1,n2);       
+}
+app.get("/sqrt", (req,res)=>{ 
+    try{
+    const n1= parseFloat(req.query.n1); 
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) { 
+        logger.error("n1 is incorrectly defined"); 
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) { 
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+
+    if(n2==0){
+        logger.error("n2 can't be zero");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    logger.info('Parameters '+n1+' and '+n2+' received for squareroot'); 
+    const result = sqrt(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+const modulo= (n1,n2) => { 
+    return n1%n2;       
+}
+app.get("/modulo", (req,res)=>{ 
+    try{
+    const n1= parseFloat(req.query.n1); 
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) { 
+        logger.error("n1 is incorrectly defined"); 
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) { 
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+
+    if(n2==0){
+        logger.error("n2 can't be zero");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    logger.info('Parameters '+n1+' and '+n2+' received for modulo operation'); 
+    const result = modulo(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+
+const port=3040;
 app.listen(port,()=>{
     console.log("i am listening to port ",port);
 })
